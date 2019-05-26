7af070c3467933259d2ef410e1f669de82a2018e
Vladimir Shkerin <vladshkerin@gmail.com>
2019-05-26 18:09:53 +0300

    Defined methods "save()" and "findById()" in ShipServiceImpl

--- src/main/java/com/space/service/ShipServiceImpl.java
+++ src/main/java/com/space/service/ShipServiceImpl.java
@@ -1,57 +1,57 @@
 package com.space.service;
 
 import com.space.model.Ship;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
+@SuppressWarnings("JpaQueryApiInspection")
+@Service("shipService")
 @Repository
 @Transactional
-@Service("shipService")
 public class ShipServiceImpl implements ShipService {
+
+    private final Log log = LogFactory.getLog(ShipServiceImpl.class);
 
     @PersistenceContext
     private EntityManager em;
 
-    @SuppressWarnings("JpaQueryApiInspection")
     @Transactional(readOnly = true)
     @Override
     public List<Ship> findAll() {
         return em.createNamedQuery("Ship.findAll", Ship.class).getResultList();
     }
 
-    @Override
-    public List<Ship> findAllWithDetail() {
-        return null;
-    }
-
-    @Override
-    public int findCountAllWithDetail() {
-        return 0;
-    }
-
+    @Transactional(readOnly = true)
     @Override
     public Ship findById(Long id) {
-        return null;
-    }
-
-    @Override
-    public Ship create() {
-        return null;
+        TypedQuery<Ship> query = em.createNamedQuery("Ship.findById", Ship.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
     }
 
     @Override
     public Ship save(Ship ship) {
-        return null;
+        if (ship.getId() == null) {
+            log.info("Inserting new ship");
+            em.persist(ship);
+        } else {
+            log.info("Updating existing ship");
+            em.merge(ship);
+        }
+        log.info("Ship saved with id: " + ship.getId());
+        return ship;
     }
 
     @Override
     public void delete(Ship ship) {
 
     }
-
 }
