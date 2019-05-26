170c34a2ac93dc70d9772415d5c24932191af510
Vladimir Shkerin <vladshkerin@gmail.com>
2019-05-26 18:03:16 +0300

    Added named queries "Ship.findById" in Ship

--- src/main/java/com/space/model/Ship.java
+++ src/main/java/com/space/model/Ship.java
@@ -1,13 +1,20 @@
 package com.space.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@SuppressWarnings("JpaQlInspection")
+@Getter
+@Setter
 @Entity
 @Table(name = "ship")
 @NamedQueries({
-        @NamedQuery(name="Ship.findAll", query="SELECT s FROM Ship s")
+        @NamedQuery(name = "Ship.findAll", query = "SELECT s FROM Ship s"),
+        @NamedQuery(name = "Ship.findById", query = "SELECT distinct s FROM Ship s WHERE s.id = :id")
 })
 public class Ship implements Serializable {
 
@@ -22,14 +29,16 @@ public class Ship implements Serializable {
     @Column(name = "planet")
     private String planet;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "shipType")
-    private String shipType;
+    private ShipType shipType;
 
+    @Temporal(TemporalType.DATE)
     @Column(name = "prodDate")
     private Date prodDate;
 
     @Column(name = "isUsed")
-    private Integer isUsed;
+    private Boolean isUsed;
 
     @Column(name = "speed")
     private Double speed;
