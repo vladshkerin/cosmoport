87801cda0de6f66930e20b5c6335a312dc3da347
Vladimir Shkerin <vladshkerin@gmail.com>
2019-06-02 09:27:17 +0300

    Renamed class from "ShipRepository" to "ShipJpaRepository"

---
+++ src/main/java/com/space/repository/ShipJpaRepository.java
@@ -0,0 +1,21 @@
+package com.space.repository;
+
+import com.space.model.Ship;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("shipJpaRepository")
+public interface ShipJpaRepository extends JpaRepository<Ship, Long>, PagingAndSortingRepository<Ship, Long> {
+
+    @Query("SELECT s FROM Ship s WHERE s.name LIKE %:name% AND s.planet LIKE %:planet%")
+    List<Ship> findAllWithFilter(@Param("name") String name, @Param("planet") String planet,
+                                 Pageable pageable);
+
+    List<Ship> findAllByNameContainingAndPlanetContaining(String name, String planet, Pageable pageable);
+}
--- src/main/java/com/space/repository/ShipRepository.java
+++
@@ -1,7 +0,0 @@
-package com.space.repository;
-
-import com.space.model.Ship;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ShipRepository extends PagingAndSortingRepository<Ship, Long> {
-}
