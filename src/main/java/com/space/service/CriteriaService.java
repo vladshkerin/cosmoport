0c597b92f4ae516e11c79b5db4f360eae2ee1b69
Vladimir Shkerin <vladshkerin@gmail.com>
2019-09-11 16:29:01 +0300

    Create bean "CriteriaService.java" for restrictions query

---
+++ src/main/java/com/space/service/CriteriaService.java
@@ -0,0 +1,115 @@
+package com.space.service;
+
+import com.space.model.Ship;
+import com.space.model.ShipType;
+import com.space.model.Ship_;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.*;
+import java.util.Date;
+import java.util.Map;
+
+@Service("criteriaService")
+public class CriteriaService {
+
+    Query createQuery(EntityManager entityManager, Map<String, String> params) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Ship> criteria = builder.createQuery(Ship.class);
+        Root<Ship> root = criteria.from(Ship.class);
+
+        criteria.select(root)
+                .where(imposeRestrictions(builder, root, params))
+                .orderBy(imposeOrder(builder, root, params));
+
+        Query query = entityManager.createQuery(criteria);
+        imposePagination(query, params);
+
+        return query;
+    }
+
+    private Order imposeOrder(CriteriaBuilder builder, Root<Ship> root, Map<String, String> params) {
+        Order order = builder.asc(root.get(Ship_.id));
+        String orderStr = String.valueOf(params.getOrDefault("order", "")).toLowerCase();
+
+        switch (orderStr) {
+            case "speed":
+                order = builder.asc(root.get(Ship_.speed));
+                break;
+            case "date":
+                order = builder.asc(root.get(Ship_.prodDate));
+                break;
+            case "rating":
+                order = builder.asc(root.get(Ship_.rating));
+                break;
+        }
+
+        return order;
+    }
+
+    private Predicate imposeRestrictions(CriteriaBuilder builder, Root<Ship> root, Map<String, String> params) {
+        String name = params.getOrDefault("name", "");
+        String planet = params.getOrDefault("planet", "");
+        ShipType shipType = ShipType.valueOf(params.getOrDefault("shipType", "EMPTY"));
+        long after = Long.parseLong(params.getOrDefault("after", "0"));
+        long before = Long.parseLong(params.getOrDefault("before", "0"));
+        String isUsedStr = params.getOrDefault("isUsed", "");
+        Double minSpeed = Double.valueOf(params.getOrDefault("minSpeed", "0.0"));
+        Double maxSpeed = Double.valueOf(params.getOrDefault("maxSpeed", "0.0"));
+        Integer minCrewSize = Integer.valueOf(params.getOrDefault("minCrewSize", "0"));
+        Integer maxCrewSize = Integer.valueOf(params.getOrDefault("maxCrewSize", "0"));
+        Double minRating = Double.valueOf(params.getOrDefault("minRating", "0.0"));
+        Double maxRating = Double.valueOf(params.getOrDefault("maxRating", "0.0"));
+
+        // Restriction
+        Predicate predicate = builder.conjunction();
+        if (!name.isEmpty()) {
+            Predicate p = builder.like(root.get(Ship_.name), "%" + name + "%");
+            predicate = builder.and(predicate, p);
+        }
+        if (!planet.isEmpty()) {
+            Predicate p = builder.like(root.get(Ship_.planet), "%" + planet + "%");
+            predicate = builder.and(predicate, p);
+        }
+        if (shipType != ShipType.EMPTY) {
+            Predicate p = builder.equal(root.get(Ship_.shipType), shipType);
+            predicate = builder.and(predicate, p);
+        }
+        if (after > 0 && before > 0) {
+            Predicate p = builder.between(root.get(Ship_.prodDate), new Date(after), new Date(before));
+            predicate = builder.and(predicate, p);
+        }
+        if (!isUsedStr.isEmpty()) {
+            Predicate p = builder.equal(root.get(Ship_.isUsed), Boolean.valueOf(isUsedStr));
+            predicate = builder.and(predicate, p);
+        }
+        if (minSpeed > 0.0 || maxSpeed > 0.0) {
+            Predicate p = builder.between(root.get(Ship_.speed), minSpeed, maxSpeed);
+            predicate = builder.and(predicate, p);
+        }
+        if (minCrewSize > 0 || maxCrewSize > 0) {
+            Predicate p = builder.between(root.get(Ship_.crewSize), minCrewSize, maxCrewSize);
+            predicate = builder.and(predicate, p);
+        }
+        if (minRating > 0 || maxRating > 0) {
+            Predicate p = builder.between(root.get(Ship_.rating), minRating, maxRating);
+            predicate = builder.and(predicate, p);
+        }
+
+        return predicate;
+    }
+
+    private void imposePagination(Query query, Map<String, String> params) {
+        int pageNumber = Integer.parseInt(params.getOrDefault("pageNumber", "0"));
+        int pageSize = Integer.parseInt(params.getOrDefault("pageSize", "3"));
+
+        if (pageNumber > 0) {
+            int number = (pageSize == 0 ? 1 : pageSize) * pageNumber;
+            query.setFirstResult(number);
+        }
+        if (pageSize > 0) {
+            query.setMaxResults(pageSize);
+        }
+    }
+}
--- src/main/java/com/space/service/ShipServiceImpl.java
+++ src/main/java/com/space/service/ShipServiceImpl.java
@@ -3,7 +3,6 @@ package com.space.service;
 import com.space.exception.BadRequestException;
 import com.space.exception.NoFoundException;
 import com.space.model.Ship;
-import com.space.model.Ship_;
 import com.space.repository.ShipJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
@@ -43,6 +38,8 @@ public class ShipServiceImpl implements ShipService {
 
     private ShipJpaRepository shipJpaRepository;
 
+    private CriteriaService criteriaService;
+
     @Transactional(readOnly = true)
     @Override
     public List<Ship> findAll() {
@@ -55,55 +52,12 @@ public class ShipServiceImpl implements ShipService {
         return shipJpaRepository.findAll(pageable);
     }
 
-    @SuppressWarnings("unchecked")
     @Transactional(readOnly = true)
+    @SuppressWarnings("unchecked")
     @Override
     public List<Ship> findAllByCriteria(Map<String, String> params) {
-        String name = params.getOrDefault("name", "");
-        String planet = params.getOrDefault("planet", "");
-//        ShipType shipType = ShipType.valueOf(params.getOrDefault("shipType", "EMPTY"));
-//        Long after = Long.valueOf(params.getOrDefault("after", "0"));
-//        Long before = Long.valueOf(params.getOrDefault("before", "0"));
-//        Boolean isUsed = Boolean.valueOf(params.getOrDefault("isUsed", "false"));
-//        Double minSpeed = Double.valueOf(params.getOrDefault("minSpeed", "0.0"));
-//        Double maxSpeed = Double.valueOf(params.getOrDefault("maxSpeed", "0.0"));
-//        Integer minCrewSize = Integer.valueOf(params.getOrDefault("minCrewSize", "0"));
-//        Integer maxCrewSize = Integer.valueOf(params.getOrDefault("maxCrewSize", "0"));
-//        Double minRating = Double.valueOf(params.getOrDefault("minRating", "0.0"));
-//        Double maxRating = Double.valueOf(params.getOrDefault("maxRating", "0.0"));
-        int pageNumber = Integer.parseInt(params.getOrDefault("pageNumber", "0"));
-        int pageSize = Integer.parseInt(params.getOrDefault("pageSize", "3"));
-
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Ship> criteria = builder.createQuery(Ship.class);
-        Root<Ship> root = criteria.from(Ship.class);
-        criteria.select(root);
-
-        Predicate predicate = builder.conjunction();
-        if (!name.isEmpty()) {
-            Predicate p = builder.like(root.get(Ship_.name), "%" + name + "%");
-            predicate = builder.and(predicate, p);
-        }
-        if (!planet.isEmpty()) {
-            Predicate p = builder.like(root.get(Ship_.planet), "%" + planet + "%");
-            predicate = builder.and(predicate, p);
-        }
-        criteria.where(predicate);
-
-        Query query = entityManager.createQuery(criteria);
-
-        // Pagination
-        if (pageNumber > 0) {
-            int number = (pageSize == 0 ? 1 : pageSize) * pageNumber;
-            query.setFirstResult(number);
-        }
-        if (pageSize > 0) {
-            query.setMaxResults(pageSize);
-        }
-
-        List<Ship> result = query.getResultList();
-
-        return result;
+        Query query = criteriaService.createQuery(entityManager, params);
+        return (List<Ship>) query.getResultList();
     }
 
     @Transactional(readOnly = true)
@@ -223,5 +177,10 @@ public class ShipServiceImpl implements ShipService {
     @Autowired
     public void setShipJpaRepository(ShipJpaRepository shipJpaRepository) {
         this.shipJpaRepository = shipJpaRepository;
+    }
+
+    @Autowired
+    public void setCriteriaService(CriteriaService criteriaService) {
+        this.criteriaService = criteriaService;
     }
 }
