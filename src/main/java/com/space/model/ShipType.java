bb97ad984220552c3de0a4fd9a0751111472fcd2
Vladimir Shkerin <vladshkerin@gmail.com>
2019-09-11 13:35:07 +0300

    Add constructor in enum ShipType.java

--- src/main/java/com/space/model/ShipType.java
+++ src/main/java/com/space/model/ShipType.java
@@ -1,7 +1,19 @@
 package com.space.model;
 
 public enum ShipType {
-    TRANSPORT,
-    MILITARY,
-    MERCHANT
+    TRANSPORT("TRANSPORT"),
+    MILITARY("MILITARY"),
+    MERCHANT("MERCHANT"),
+    EMPTY("");
+
+    private final String shipType;
+
+    ShipType(String shipType) {
+        this.shipType = shipType;
+    }
+
+    @Override
+    public String toString() {
+        return this.shipType;
+    }
 }
